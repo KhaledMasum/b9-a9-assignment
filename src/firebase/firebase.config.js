8e4f0b7cd210f5584947869ebbe77e80dd60785a
Khaled Masum <khaledmasum1@gmail.com>
2024-07-15 01:47:34 +0600

    Setup the project

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_Yp1v-G5R6yiItxJ565njNwJuAhRhmS4",
+  authDomain: "b9-a9-assignment.firebaseapp.com",
+  projectId: "b9-a9-assignment",
+  storageBucket: "b9-a9-assignment.appspot.com",
+  messagingSenderId: "516020274557",
+  appId: "1:516020274557:web:e0d60337c69924902df814"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,18 @@
+import Root from '../layouts/Root/Root';
+import { createBrowserRouter } from "react-router-dom";
+import Home from '../pages/Home/Home';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root></Root>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    },
+]);
+
+export default router;
