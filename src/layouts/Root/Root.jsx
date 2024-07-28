c182dc4e8228d1580983e8622bb2fa735f22ab55
Khaled Masum <khaledmasum1@gmail.com>
2024-07-28 23:56:32 +0600

    Added footer section

--- src/layouts/Root/Root.jsx
+++ src/layouts/Root/Root.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../pages/Navbar";
 import { ToastContainer } from "react-toastify";
+import Footer from "../../pages/Footer";
 
 const Root = () => {
     return (
-        <div className="max-w-6xl mx-auto">
+        <div>
             <Navbar></Navbar>
             <ToastContainer />
-            <Outlet></Outlet>
+            <Outlet className="max-w-6xl mx-auto"></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
